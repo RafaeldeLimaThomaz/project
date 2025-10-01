3742e8da31e14edd78bda9a0ffe7c9ae121bad95
Rafael de Lima Thomaz <79thomaz@gmail.com>
2025-10-01 18:25:37 -0300

    add first route

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,13 +1,39 @@
 import { env } from '@/env'
-import { fastifyCors } from "@fastify/cors";
-import { fastify } from "fastify";
+import { uploadImageRoute } from '@/infra/http/routes/upload-image'
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
-const server = fastify();
+const server = fastify()
 
-server.register(fastifyCors, { origin: "*" });
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  // Envia o erro p/ alguma ferramenta de observabilidade (Sentry/DataDog/Grafana/OTel)
+
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
+
+server.register(fastifyCors, { origin: '*' })
+
+server.register(uploadImageRoute)
 
 console.log(env.DATABASE_URL)
 
-server.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
-	console.log("HTTP Server running!");
-});
+server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log('HTTP Server running!')
+})
